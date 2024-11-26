f928f5d8c21bdddc47cf48e9ed0cd6349b41ddf4
kadampratikengg <kadampratik.engg@gmail.com>
2024-11-26 16:30:04 +0530

    26-11

--- main.tf
+++ main.tf
@@ -4,5 +4,19 @@ terraform {
         source = "hashicorp/aws"
         version = "5.77.0"
     }
+    
+  } 
+}
+
+provider "aws" {
+  
+}
+
+resource "aws_s3_bucket" "myapp_bucket" {
+  bucket = "my-tf-test-bucket"
+  tags = {
+    Name        = "MyAppBucket"
+    Environment = "Dev"
   }
+
 }
